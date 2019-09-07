58d8ada05176c2d6dc7da8e25441c1900a605ab1
Daniel <dejong543@gmail.com>
2019-09-07 22:37:21 +0200

    First test deploy

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const path = require("path");
+app.use(express.static(path.resolve(__dirname, "./finatra/dist/")));
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/finatra/dist"));
+  res.sendFile(path.resolve(__dirname, "./finatra/dist/index.html"));
 });
-app.listen(process.env.PORT);
+app.listen(3000, () => console.log("yes"));
+console.log(path.resolve(__dirname, "./finatra/dist"));
